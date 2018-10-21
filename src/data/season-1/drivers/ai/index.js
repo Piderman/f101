4c815107f8db09d1bb8db02ba8665558092c2811
Matty <matty@mattycollins.com.au>
2018-10-21 13:01:32 +1100

    add: red bull ai

--- src/data/season-1/drivers/ai/index.js
+++ src/data/season-1/drivers/ai/index.js
@@ -1,7 +1,10 @@
-import Raikokonen from "./raikkonen"
-import Alonso from "./alonso"
+import Raikokonen from "./raikkonen";
+import Alonso from "./alonso";
 
 import Bottas from "./bottas";
 import Massa from "./massa";
 
-export default [Bottas, Massa, Raikokonen, Alonso];
+import Ricciardo from "./ricciardo";
+import Vettel from "./vettel";
+
+export default [Bottas, Massa, Raikokonen, Alonso, Ricciardo, Vettel];
---
+++ src/data/season-1/drivers/ai/vettel/index.js
@@ -0,0 +1,8 @@
+import races from "./races";
+
+export default {
+  driverName: "Sebastian Vettel",
+  driverType: "ai",
+  primaryTeamId: 2,
+  races
+};
